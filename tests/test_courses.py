eeed5ba0e3506c80a6df6894e523002934848c47
iinj <154603765+iinj@users.noreply.github.com>
2025-08-21 21:58:10 +0300

    Add PageComponents and integrate into pages

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -10,11 +10,10 @@ from pages.courses_list_page import CoursesListPage
 def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
     create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
 
-    create_course_page.check_visible_create_course_title()
-    create_course_page.check_disabled_create_course_button()
+    create_course_page.create_course_toolbar_view.check_visible()
 
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-    create_course_page.check_visible_create_course_form(
+    create_course_page.create_course_form.check_visible(
         title='',
         estimated_time='',
         description='',
@@ -22,20 +21,19 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
         min_score='0'
 
     )
-    create_course_page.check_visible_exercises_title()
-    create_course_page.check_visible_create_exercise_button()
+    create_course_page.create_course_exercises_toolbar_view.check_visible()
     create_course_page.check_visible_exercises_empty_view()
 
     create_course_page.image_upload_widget.upload_preview_image('testdata/files/image_1.jpg')
     create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-    create_course_page.fill_create_course_form(
+    create_course_page.create_course_form.fill(
         title='Playwright',
         estimated_time='2 weeks',
         description='Playwright',
         max_score='100',
         min_score='10'
     )
-    create_course_page.click_create_course_button()
+    create_course_page.create_course_toolbar_view.click_create_course_button()
 
     courses_list_page.toolbar_view.check_visible()
     courses_list_page.course_view.check_visible(
