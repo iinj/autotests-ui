3856053bc37692fa86526ea05ba29c02e263f028
iinj <154603765+iinj@users.noreply.github.com>
2025-08-22 17:12:02 +0300

    Add test_edit_course

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -44,7 +44,7 @@ class TestCourses:
             estimated_time='2 weeks'
         )
 
-    def test_empty_courses_list(self,courses_list_page: CoursesListPage):
+    def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
         courses_list_page.navbar.check_visible('username')
@@ -52,3 +52,41 @@ class TestCourses:
 
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+        create_course_page.image_upload_widget.upload_preview_image('testdata/files/image_1.jpg')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title='Playwright',
+            estimated_time='2 weeks',
+            description='Playwright',
+            max_score='100',
+            min_score='10'
+        )
+
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(index=0,
+                                                    title='Playwright',
+                                                    estimated_time='2 weeks',
+                                                    max_score='100',
+                                                    min_score='10'
+                                                    )
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        create_course_page.create_course_form.fill(
+            title='New Playwright',
+            estimated_time='3 weeks_edit',
+            description='New Playwright',
+            max_score='1000',
+            min_score='100'
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(index=0,
+                                                    title='New Playwright',
+                                                    estimated_time='3 weeks_edit',
+                                                    max_score='1000',
+                                                    min_score='100')
