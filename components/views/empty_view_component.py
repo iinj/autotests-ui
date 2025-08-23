f327d206a6bf9e5dcfff1a4ac52b67e7cbf83a1f
iinj <154603765+iinj@users.noreply.github.com>
2025-08-23 15:59:16 +0300

    Add allure steps in tests

--- components/views/empty_view_component.py
+++ components/views/empty_view_component.py
@@ -1,3 +1,5 @@
+import allure
+
 from components.base_component import BaseComponent
 from playwright.sync_api import Page
 
@@ -13,6 +15,7 @@ class EmptyViewComponent(BaseComponent):
         self.title = Text(page, f'{identifier}-empty-view-title-text', 'Title')
         self.description = Text(page, f'{identifier}-empty-view-description-text', 'Description')
 
+    @allure.step('Check visible empty view "{title}"')
     def check_visible(self, title: str, description: str):
         self.icon.check_visible()
 
---
+++ tests/pytest/test_allure_pytest.py
@@ -0,0 +1,25 @@
+import allure
+
+
+@allure.step('Opening browser')
+def open_browser():
+    with allure.step('Get browser'):
+        ...
+
+    with allure.step('Start browser'):
+        ...
+
+
+@allure.step('Creating course with title: {title}')
+def create_course(title: str):        ...
+
+
+@allure.step('Closing browser')
+def close_browser():
+    ...
+
+
+def test_feature():
+    open_browser()
+    create_course()
+    close_browser()
