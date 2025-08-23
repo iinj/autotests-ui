4632088a196e39913684f23c889e7a686773653c
iinj <154603765+iinj@users.noreply.github.com>
2025-08-23 17:26:34 +0300

    Refactoring fixtures, add record video to allure

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,14 +1,15 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page, Playwright
+
+from playwright.sync_api import Page, Playwright
+from _pytest.fixtures import SubRequest
 
 from pages.authentication.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture
-def chromium_page(playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
-    browser.close()
+def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 
 @pytest.fixture(scope='session')
@@ -19,7 +20,7 @@ def initialize_browser_state(playwright: Playwright):
 
     registration_page = RegistrationPage(page=page)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.registration_form.fill(email='user.name@gmail.com', username='username',password='password')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
     registration_page.registration_button.click()
 
     context.storage_state(path='browser-state.json')
@@ -27,8 +28,8 @@ def initialize_browser_state(playwright: Playwright):
 
 
 @pytest.fixture(scope='function')
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
-    yield context.new_page()
-    browser.close()
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state='browser-state.json')
