b05f3ff0e070f823f6cd71c793c205bf881aab5e
iinj <154603765+iinj@users.noreply.github.com>
2025-08-26 15:17:18 +0300

    Add environment variable to allure report

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from typing import Self
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import EmailStr, FilePath, HttpUrl, DirectoryPath, BaseModel
 
-from tests.pytest.test_fixtures import settings
-
 
 class Browser(str, Enum):
     WEBKIT = 'webkit'
@@ -37,21 +35,25 @@ class Settings(BaseSettings):
     test_data: TestData
     videos_dir: DirectoryPath
     tracing_dir: DirectoryPath
+    allure_results_dir: DirectoryPath
     browser_state_file: FilePath
 
     @classmethod
     def initialize(cls) -> Self:
         videos_dir = DirectoryPath('./videos')
         tracing_dir = DirectoryPath('./tracing')
+        allure_results_dir = DirectoryPath('./allure-results')
         browser_state_file = FilePath('browser-state.json')
 
         videos_dir.mkdir(exist_ok=True)
         tracing_dir.mkdir(exist_ok=True)
+        allure_results_dir.mkdir(exist_ok=True)
         browser_state_file.touch(exist_ok=True)
 
         return Settings(
             videos_dir=videos_dir,
             tracing_dir=tracing_dir,
+            allure_results_dir=allure_results_dir,
             browser_state_file=browser_state_file
         )
 
