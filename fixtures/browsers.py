31b32df5a45c7ed1c4194123c7b4f998cf756414
iinj <154603765+iinj@users.noreply.github.com>
2025-08-26 17:24:43 +0300

    Add GitHub Actions workflows

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -18,7 +18,7 @@ def page(request: SubRequest, playwright: Playwright) -> Generator[Any, None, No
         browser_type=request.param)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='session ')
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=settings.headless)
     context = browser.new_context(base_url=settings.get_base_url())
